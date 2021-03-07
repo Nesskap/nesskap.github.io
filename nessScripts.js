c566ba10b9113a4e5779131afaeefec23bda0289
Nesskap <79773836+Nesskap@users.noreply.github.com>
2021-03-06 21:30:07 -0600

    Add files via upload

--- nessScripts.js
+++ nessScripts.js
@@ -1,8 +1,17 @@
 function menuDropdown() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+function passwordCheck(){
+  if(document.getElementById('password').value == 'onlyyouknowit'){
+      return true;
+  }else{
+    alert('Wrong Password');
+    return false;
+  }
+}
